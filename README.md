393da84c7fb481d5f788bee3deced8d5f172569e
David Johnson <djohnson@tradeking.com>
2017-06-02 12:02:48 -0400

    Major refactor for better flow for single line input
    - Allows multiple issues on the same line, except for Git branch
    formatting
    - Updates flags to be more consistent and declarative

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # jiraDetails
-
-##Installation
+## Installation
   `npm install -g https://github.com/djak250/jiraDetails`
 
   `export JIRA_DOMAIN="{your_jira_domain}"`
@@ -18,9 +17,37 @@
 ## Usage
 `git branch | jiraDetails`
 
+## Arguments
+- `--git`
+-- Use this option when passing in git branch out. Will return the branches in the same format with the jira summaries to the end of each line
+-- NOTE: This will only match the first issue per line, to remain consistent with a single line for each branch. Eg., feature/ABC-1234-and-DEF-5678 will only match the ABC-1234, and display it's summary.
+  ```shell
+  * feature/ABC-100 | Detail JiraDetails
+    feature/ABC-101 | Document JiraDetails
+    feature/DEF-201 | Implement JiralDetails
+    master
+    nonJiraBranch
+  ```
+- `--html`
+-- Outputs issues in an unordered list formatted in HTML
+    ```html
+    <ul>
+        <li><b>TKA-1234</b> - Fix the problem with IRA's not being able to fill out the information</li>
+    </ul>
+    ```
+
+- `--markdown`
+-- Outputs issues in an unordered list formatted in Markdown
+    ```
+    - **TKA-1234** - Fix the problem with IRA's not being able to fill out the information
+    ```
+
+- `--summary`
+-- Outputs only the summary for the input issues
+
 ## Example
 ```shell
-$ git branch | jiraDetails
+$ git branch | jiraDetails --git
 * feature/ABC-100 | Detail JiraDetails
   feature/ABC-101 | Document JiraDetails
   feature/DEF-201 | Implement JiralDetails
--- index.js
+++ index.js
@@ -1,53 +1,61 @@
 #! /usr/bin/env node
 'use strict';
 
-const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
-let htmlOutput = false;
-let markDownOutput = false;
-let issueOnly = false;
-for (let i = 0; i < process.argv.length; i++) {
-    if (process.argv[i] === '--html') {
-        htmlOutput = true;
-    }
-    if (process.argv[i] === '--markdown') {
-        markDownOutput = true;
-    }
-    if (process.argv[i] === '--issue-only') {
-        issueOnly = true;
-    }
-}
-const stdin = process.stdin;
-const stdout = process.stdout;
-const stderr = process.stderr;
 const request = require('request');
 const fs = require('fs');
 const columnify = require('columnify');
-const columnConfig = {
+
+const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
+const STDIN = process.stdin;
+const STDOUT = process.stdout;
+const STDERR = process.stderr;
+const COLUMNCONFIG = {
     showHeaders: false,
-    columns: ['current', 'branch', 'issue']
+    columns: ['current', 'issue', 'summary']
 };
+
+let READ_DATA = false;
+
+let GIT_OUTPUT = false;
+let gitBranches = [];
+const gitBranchMap = new Map();
+
+let HTML_OUTPUT = false;
+let MARKDOWN_OUTPUT = false;
+let SUMMARY_OUTPUT = false;
+
+for (let i = 0; i < process.argv.length; i++) {
+    if (process.argv[i] === '--git') {
+        GIT_OUTPUT = true;
+    } else if (process.argv[i] === '--html') {
+        HTML_OUTPUT = true;
+    } else if (process.argv[i] === '--markdown') {
+        MARKDOWN_OUTPUT = true;
+    } else if (process.argv[i] === '--summary') {
+        SUMMARY_OUTPUT = true;
+    }
+}
 
 let error = false;
 if (!process.env.JIRA_DOMAIN) {
-    stderr.write('Missing JIRA_DOMAIN env variable\n');
+    STDERR.write('Missing JIRA_DOMAIN env variable\n');
     error = true;
 }
 if (!process.env.JIRA_USER) {
-    stderr.write('Missing JIRA_USER env variable\n');
+    STDERR.write('Missing JIRA_USER env variable\n');
     error = true;
 }
 if (!process.env.JIRA_PW) {
-    stderr.write('Missing JIRA_PW env variable\n');
+    STDERR.write('Missing JIRA_PW env variable\n');
     error = true;
 }
 if (process.env.NODE_TLS_REJECT_UNAUTHORIZED !== '0') {
-    stderr.write(`Please set NODE_TLS_REJECT_UNAUTHORIZED='0'\n`);
+    STDERR.write('Please set NODE_TLS_REJECT_UNAUTHORIZED=\'0\'\n');
     error = true;
 }
 if (error) {
     return;
 }
-
 
 const headers = {
     'Content-Type': 'application/json',
@@ -58,8 +66,28 @@ const headers = {
         password: process.env.JIRA_PW
     }
 };
-let gitBranches = [];
-let gitBranchesSanitized = [];
+
+const loadCache = function(cb) {
+    fs.readFile(`${__dirname}/.jira.cache`, 'utf8', (err, fileOutput) => {
+        if (fileOutput) {
+            const cachedIssuesMap = new Map(
+                fileOutput.trim().split('\n')
+                .map((line) => line.split('|'))
+            );
+            return cb(cachedIssuesMap);
+        }
+        cb(new Map());
+    });
+};
+
+const appendCache = function(missingIssueMap, cb) {
+    fs.appendFile(
+        `${__dirname}/.jira.cache`,
+        `${Array.from(missingIssueMap).map((i) => i.join('|')).join('\n')}\n`,
+        'utf8',
+        cb
+    );
+};
 
 const buildJQL = function(keys) {
     let jql = '';
@@ -73,84 +101,7 @@ const buildJQL = function(keys) {
     return jql;
 };
 
-
-const buildBranchList = function() {
-    fs.readFile(`${__dirname}/.jira.cache`, 'utf8', (err, fileOutput) => {
-        if (fileOutput) {
-            const cachedIssuesMap = new Map();
-            const cachedIssuesArray = !!fileOutput ? fileOutput.trim().split('\n') : [];
-            const cachedIssueKeys = [];
-            for (let i = 0; i < cachedIssuesArray.length; i++) {
-                const issue = cachedIssuesArray[i].split('|');
-                cachedIssueKeys.push(issue[0]);
-                cachedIssuesMap.set(issue[0], issue[1]);
-            }
-            const formattedJiraBranches = gitBranches.map((gb) => {
-                if (/\S-\d/.test(gb)) {
-                    const sanitizedBranch = gb.trim().replace(/^\*\s*/, '').replace(/^\S*\//, '');
-                    return {
-                        branch: gb,
-                        issue: cachedIssuesMap.get(sanitizedBranch)
-                    };
-                }
-                return { branch: gb };
-            }).map((gb) => {
-                if (gb.branch.indexOf('*') === 0) {
-                    return Object.assign({}, gb, {
-                        current: '*',
-                        branch: `\x1b[32m${gb.branch.replace('* ', '')}\x1b[0m`
-                    });
-                }
-                return gb;
-            });
-            let output = `${columnify(formattedJiraBranches, columnConfig)}`;
-            if (htmlOutput) {
-                output = [
-                    `<ul>\n${output.split('\n').map((line) => (`\t<li>${line.trim()}</li>`)).join('\n')}`,
-                    '</ul>'
-                ].join('\n');
-            } else if (markDownOutput) {
-                output = output.split('\n').map((line) => {
-                    const match = line.match(/\w+-\d+/);
-                    if (match) {
-                        let featureContents = line.split(' ');
-                        featureContents = featureContents.slice(featureContents.indexOf(match[0]) + 1);
-                        return `**${match[0].replace(/feature\//, '')}** - ${featureContents.join(' ')}`;
-                    }
-                    return line;
-                }).join('\n');
-            } else if (issueOnly) {
-                stdout.write(`${formattedJiraBranches.map((b) => b.issue).join('\n')}`);
-                return;
-            }
-            stdout.write(`${output}\n`);
-        } else {
-            gitBranches = gitBranches.map((gb) => {
-                if (gb.indexOf('*') === 0) {
-                    return {
-                        current: '*',
-                        branch: `\x1b[32m${gb.replace('* ', '')}\x1b[0m`
-                    };
-                }
-                return { branch: gb };
-            });
-            let output = `${columnify(gitBranches, columnConfig)}`;
-            if (htmlOutput) {
-                output = [
-                    `<ul>\n${output.split('\n').map((line) => (`\t<li>${line.trim()}</li>`)).join('\n')}`,
-                    '</ul>'
-                ].join('\n');
-            }
-            if (issueOnly) {
-                stdout.write(`${gitBranches.map((b) => b.issue).join('\n')}`);
-            } else {
-                stdout.write(`${output}\n`);
-            }
-        }
-    });
-};
-
-function fetchMissingIssues(missingKeys, errCb) {
+function fetchMissingIssues(missingKeys, cb, errCb) {
     if (missingKeys.length !== 0) {
         headers.json = {
             jql: buildJQL(missingKeys),
@@ -176,58 +127,89 @@ function fetchMissingIssues(missingKeys, errCb) {
                     return null;
                 }));
                 const newMissingKeys = missingKeys.filter((mk) => nonExistingKeys.indexOf(mk) === -1);
-                return fetchMissingIssues(newMissingKeys, errCb);
+                return fetchMissingIssues(newMissingKeys, cb, errCb);
             }
             if (!body.issues || !body.issues.length) return errCb(new Error('EMPTY BODY\n'));
-            const issueMap = new Map();
-            for (let i = 0; i < body.issues.length; i++) {
-                const issue = body.issues[i];
-                issueMap.set(issue.key, issue.fields.summary);
-            }
-            fs.appendFile(
-                `${__dirname}/.jira.cache`,
-                `${Array.from(issueMap).map((i) => (`${i[0]}|${i[1]}`)).join('\n')}\n`,
-                'utf8',
-                buildBranchList
-            );
+            const missingIssueMap = new Map(body.issues.map((issue) => [issue.key, issue.fields.summary]));
+            appendCache(missingIssueMap, cb);
         });
     } else {
-        return buildBranchList();
+        cb();
     }
 }
 
-let piped = false;
-stdin.on('readable', () => {
-    const chunk = stdin.read();
-    if (chunk !== null) {
-        piped = true;
-        gitBranches = chunk.toString().trim().split('\n');
-        gitBranchesSanitized = chunk
-            .toString()
-            .replace(/[ ,*]/g, '')
-            .trim()
-            .split('\n');
-        const jiraIssues = gitBranchesSanitized
-            .filter((b) => /\S-\d/.test(b))
-            .map((b) => b.replace(/^\S*\//, ''));
+const printToScreen = function(issuesToPrintMap /* { Issue: Summary} */) {
+    if (GIT_OUTPUT) {
+        const gitOutputArray = [];
+        for (let i = 0, l = gitBranches.length; i < l; i++) {
+            const gitBranch = gitBranches[i];
+            const gitBranchObj = {
+                current: gitBranch.indexOf('*') === 0 ? '*' : '',
+                issue: gitBranch.replace(/^\* /, ''),
+                summary: issuesToPrintMap.get(gitBranchMap.get(gitBranch)) || ''
+            };
+            gitOutputArray.push(gitBranchObj);
+        }
+        STDOUT.write(columnify(gitOutputArray, COLUMNCONFIG));
+    } else {
+        const issuesArrayObject = Array.from(issuesToPrintMap.entries()).map(([issue, summary]) => ({ issue: issue, summary: summary }));
+        if (HTML_OUTPUT) {
+            let output = '<ul>\n';
+            output += issuesArrayObject.map((issue) => `\t<li><b>${issue.issue}</b> - ${issue.summary}</li>`).join('\n');
+            output += '\n</ul>';
+            STDOUT.write(output);
+        } else if (MARKDOWN_OUTPUT) {
+            const output = issuesArrayObject.map((issue) => `- **${issue.issue}** - ${issue.summary}`);
+            STDOUT.write(output.join('\n'));
+        } else if (SUMMARY_OUTPUT /* Only prints out summary text*/) {
+            STDOUT.write(Array.from(issuesToPrintMap.values()).join('\n'));
+        } else {
+            STDOUT.write(columnify(issuesArrayObject, COLUMNCONFIG));
+        }
+    }
+    STDOUT.write('\n');
+};
 
-        fs.readFile(`${__dirname}/.jira.cache`, 'utf8', (err, fileOutput) => {
-            const cachedIssuesMap = new Map();
-            const cachedIssuesArray = !!fileOutput ? fileOutput.trim().split('\n') : [];
-            const cachedIssueKeys = [];
-            for (let i = 0; i < cachedIssuesArray.length; i++) {
-                const issue = cachedIssuesArray[i].split('|');
-                cachedIssueKeys.push(issue[0]);
-                cachedIssuesMap.set(issue[0], issue[1]);
-            }
-            const missingKeys = jiraIssues.filter((ji) => cachedIssueKeys.indexOf(ji) === -1);
-            fetchMissingIssues(missingKeys, (fetchErr) => {
-                if (LOG_LEVEL === 'ERROR') stderr.write(fetchErr.toString());
-                buildBranchList();
+STDIN.on('readable', () => {
+    const chunk = STDIN.read();
+    if (chunk !== null) {
+        READ_DATA = true; // Flag to prevent exit as data has been read regardless of future null reads
+        let issueKeys = [];
+        if (GIT_OUTPUT) {
+            gitBranches = gitBranches.concat(chunk.toString().trim().split('\n'));
+            issueKeys = gitBranches
+                .filter((gb) => /\w+-\d+/.test(gb))
+                .map((gb) => {
+                    // Adds the jira issue ticket to the branch description key
+                    gitBranchMap.set(gb, gb.match(/\w+-\d+/)[0].toUpperCase());
+                    return gb.match(/\w+-\d+/)[0].toUpperCase();
+                });
+        } else {
+            issueKeys = chunk.toString().trim().match(/\w+-\d+/g) || [];
+            issueKeys = issueKeys.map(i => i.toUpperCase());
+        }
+
+        loadCache((cachedIssuesMap) => {
+            const cachedIssueKeys = Array.from(cachedIssuesMap.keys());
+            const missingKeys = issueKeys.filter((ji) => cachedIssueKeys.indexOf(ji) === -1);
+            fetchMissingIssues(missingKeys,
+            () => {
+                loadCache((issues) => {
+                    const outputIssueMap = new Map();
+                    for (let i = 0, l = issueKeys.length; i < l; i++) {
+                        const issue = issueKeys[i];
+                        if (issues.has(issue)) {
+                            outputIssueMap.set(issue, issues.get(issue));
+                        }
+                    }
+                    printToScreen(outputIssueMap);
+                });
+            }, (fetchError) => {
+                if (LOG_LEVEL === 'ERROR') STDERR.write(fetchError.toString());
             });
         });
-    } else if (!piped) {
-        stderr.write('Usage: git branch | jiraDetails\n');
+    } else if (!READ_DATA) {
+        STDERR.write('Usage: git branch | jiraDetails\n');
         process.exit(1);
     }
 });
